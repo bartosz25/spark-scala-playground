87fbcb0b6be396aaaa20a03d74a6034602504b55
bartosz25 <bartkonieczny@yahoo.fr>
2019-06-08 08:00:01 +0200

    remove meaningfulness parameters

--- src/test/scala/com/waitingforcode/memoryimpact/OomParallelWithSplitSizeDefined.scala
+++ src/test/scala/com/waitingforcode/memoryimpact/OomParallelWithSplitSizeDefined.scala
@@ -6,10 +6,6 @@ object OomParallelWithSplitSizeDefined {
   def main(args: Array[String]): Unit = {
     prepareTestEnvironment()
     val sparkSession = parallelProcessingSession
-    sparkSession.sparkContext
-      .hadoopConfiguration.set("mapreduce.input.fileinputformat.split.minsize", (OneGbInBytes/100).toString) // 10MB
-    sparkSession.sparkContext
-      .hadoopConfiguration.set("mapreduce.input.fileinputformat.split.maxsize", (OneGbInBytes/100).toString) // 10MB
     val textRdd = sparkSession.sparkContext.textFile(Test1GbFile)
     processTextRdd(textRdd)
   }
--- src/test/scala/com/waitingforcode/memoryimpact/SequentialProcessing.scala
+++ src/test/scala/com/waitingforcode/memoryimpact/SequentialProcessing.scala
@@ -5,10 +5,6 @@ object SequentialProcessing {
   def main(args: Array[String]): Unit = {
     prepareTestEnvironment()
     val sparkSession = sequentialProcessingSession
-    sparkSession.sparkContext
-      .hadoopConfiguration.set("mapreduce.input.fileinputformat.split.minsize", (OneGbInBytes/100).toString) // 10MB
-    sparkSession.sparkContext
-      .hadoopConfiguration.set("mapreduce.input.fileinputformat.split.maxsize", (OneGbInBytes/100).toString) // 10MB
     val textRdd = sparkSession.sparkContext.textFile(Test1GbFile)
     processTextRdd(textRdd)
   }
