6d2b274eccdf94856a0d2f99cf4fcd534305b59f
bartosz25 <bartkonieczny@yahoo.fr>
2019-07-05 10:06:34 +0200

    remove unused class

--- src/test/scala/com/waitingforcode/structuredstreaming/join/JoinTestHelper.scala
+++ src/test/scala/com/waitingforcode/structuredstreaming/join/JoinTestHelper.scala
@@ -3,8 +3,6 @@ package com.waitingforcode.structuredstreaming.join
 import java.sql.Timestamp
 import java.util.concurrent.atomic.AtomicInteger
 
-import org.apache.spark.sql.execution.streaming.MemoryStream
-import org.apache.spark.sql.streaming.StreamingQuery
 import org.apache.spark.sql.{ForeachWriter, Row}
 
 import scala.collection.mutable
@@ -33,30 +31,6 @@ object TestedValuesContainer {
   val values = new mutable.ListBuffer[JoinResult]()
 
   val key1Counter = new AtomicInteger(0)
-}
-
-class OneKeyDataSender(query: StreamingQuery, mainEventsStream: MemoryStream[MainEvent],
-                       joinedEventsStream: MemoryStream[JoinedEvent]) {
-
-  val thread = new Thread(new Runnable() {
-    override def run(): Unit = {
-      while (!query.isActive) {}
-      var wasSent = false
-      while (query.isActive) {
-        Thread.sleep(1000L)
-        if (!wasSent) {
-          wasSent = true
-          val mainEventTime = System.currentTimeMillis()
-          mainEventsStream.addData(MainEvent(s"key1", mainEventTime, new Timestamp(mainEventTime)))
-        }
-        // Logically key1 should expire after 5 seconds so joined key shouldn't be really joined because of the watermark
-        // on the main event
-        joinedEventsStream.addData(Events.joined(s"key1"))
-        TestedValuesContainer.key1Counter.incrementAndGet()
-      }
-    }
-  })
-
 }
 
 case class MainEvent(mainKey: String, mainEventTime: Long, mainEventTimeWatermark: Timestamp) {
